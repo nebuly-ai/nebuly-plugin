7fa088adbeab99589f8c5921ca4aab5f46e64111
diegofiori <d.fiori@nebuly.ai>
2023-11-26 17:34:48 +0100

    chore: remove unnecessary logs

--- background.js
+++ background.js
@@ -2,13 +2,13 @@ console.log("Background script running");
 
 chrome.webRequest.onCompleted.addListener(
     function(details) {
-        console.log("Request completed:", details);
+        // console.log("Request completed:", details);
         // Inject the content script
         if (details.url === "https://chat.openai.com/backend-api/conversation") {
             console.log(details)
             chrome.tabs.executeScript(details.tabId, {file: 'content.js'});
         }
-        console.log(details.url);   
+        // console.log(details.url);   
     },
     // filters
     {
@@ -20,10 +20,10 @@ chrome.webRequest.onCompleted.addListener(
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         if (details.method === "POST" && details.requestBody && details.requestBody.raw && details.url === "https://chat.openai.com/backend-api/conversation/message_feedback") {
-            console.log("Request intercepted:", details);
+            // console.log("Request intercepted:", details);
             var postedString = decodeURIComponent(String.fromCharCode.apply(null, new Uint8Array(details.requestBody.raw[0].bytes)));
             var postedObject = JSON.parse(postedString);
-            console.log(postedObject);
+            // console.log(postedObject);
             chrome.tabs.sendMessage(details.tabId, {action: postedObject.rating, message_id: postedObject.message_id});
         }
     },
